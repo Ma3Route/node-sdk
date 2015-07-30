3deb017db59232c68a938662c5c3ccef528bd9f5
GochoMugo <mugo@forfuture.co.ke>
2015-07-30 14:37:47 +0300

    (poller) allow async params func and error event
    Change:
      User needs to explicitly call `Poller#start` to start the poller.
    
      A user can pass an asynchronous function that is passed a
      `done` function as the only argument, that the async function
      should invoke with the return value (params) when done.
    
      For example,
      ```js
      new Poller(getRequest, {
        params: function(done) {
          asyncAction(done);
        },
      });
      ```
    
      The poller automatically tracks the last read id for us! The id of
      the last item in the items list returned in a successful get request
      is stored as the last read id. Subsequent requests will use this id
      as the last read id!

--- test/test.poller.js
+++ test/test.poller.js
@@ -50,19 +50,21 @@ describe("Poller", function() {
             poller.stop();
             return done();
         }, { interval: 100 });
+        poller.start();
     });
 
     it("options.params are passed to the callback", function(done) {
         var options = { params: { } };
         var poller = new Poller(function(params) {
             poller.stop();
-            should.strictEqual(params, options.params);
+            should.deepEqual(params, options.params);
             return done();
         }, options);
+        poller.start();
     });
 
     it("calls options.params if it is a function", function(done) {
-        var tracker = { };
+        var tracker = { myname: "gocho" };
         var options = {
             params: function() {
                 return tracker;
@@ -70,9 +72,39 @@ describe("Poller", function() {
         };
         var poller = new Poller(function(params) {
             poller.stop();
-            should.strictEqual(params, tracker);
+            should.deepEqual(params, tracker);
             return done();
         }, options);
+        poller.start();
+    });
+
+    it("sets the lastreadId automatically", function(done) {
+        var id = 1;
+        var poller = new Poller(function(params, callback) {
+            should(params.lastreadId).eql(id++);
+            if (id === 5) {
+                poller.stop();
+                return done();
+            }
+            return callback(null, [{ id: id }]);
+        }, { params: { lastreadId: id }, interval: 10 });
+        poller.start();
+    });
+
+    it("allows an asynchronous params function", function(done) {
+        var tracker = { track: "no one" };
+        var options = {
+            params: function params(amDone) {
+                return amDone(tracker);
+            },
+        };
+        var poller = new Poller(function(params, callback) {
+            poller.stop();
+            should.deepEqual(params, tracker);
+            callback(null, []);
+            return done();
+        }, options);
+        poller.start();
     });
 });
 
@@ -80,51 +112,89 @@ describe("Poller", function() {
 describe("Poller#on", function() {
     it("adds a listener", function(done) {
         var poller = new Poller(function(params, callback) {
-            return callback();
+            return callback(null, "data");
         });
         poller.on("message", function() {
             poller.stop();
             return done();
         });
+        poller.start();
     });
 
-    it("listener is passed arguments from get request", function(done) {
-        var err = new Error();
+    it("event.message: data is passed", function(done) {
         var items = [ ];
         var meta = [ ];
         var res = { };
         var poller = new Poller(function(params, callback) {
-            return callback(err, items, meta, res);
+            return callback(null, items, meta, res);
         });
-        poller.on("message", function(retErr, retItems, retMeta, retRes) {
+        poller.on("message", function(retItems, retMeta, retRes) {
             poller.stop();
-            should.strictEqual(retErr, err);
             should.strictEqual(retItems, items);
             should.strictEqual(retMeta, meta);
             should.strictEqual(retRes, res);
             return done();
         });
+        poller.start();
+    });
+
+    it("event.error: error is passed", function(done) {
+        var error = new Error();
+        var poller = new Poller(function(params, callback) {
+            return callback(error);
+        });
+        poller.on("error", function(err) {
+            poller.stop();
+            should.strictEqual(err, error);
+            return done();
+        });
+        poller.start();
+    });
+
+    it.skip("event.error: throws an error to the process if no listener", function(done) {
+        /**
+         * See http://www.adaltas.com/blog/2013/03/27/test-uncaughtException-error-mocha/
+         */
+        var poller;
+        var listeners = process.listeners();
+        process.removeAllListeners("uncaughtException");
+        var handler = function handler() {
+            poller.stop();
+            process.removeAllListeners("uncaughtException");
+            listeners.forEach(function(listener) {
+                process.on("uncaughtException", listener);
+            });
+            return done();
+        };
+        process.on("uncaughtException", handler);
+        poller = new Poller(function(params, callback) {
+            return callback(new Error());
+        });
+        poller.start();
     });
 });
 
 
 describe("Poller#stop", function() {
     it("stops the timer", function(done) {
-        var num = 0;
         var poller = new Poller(function(p, callback) {
-            return callback(++num);
+            return callback(null, []);
         }, { interval: 100 });
-        poller.on("message", function(retNum) {
-            should(retNum).below(2);
+        poller.on("message", function() {
             poller.stop();
             return done();
         });
+        poller.start();
     });
 
-    it("removes all listeners", function() {
-        var poller = new Poller(noop);
+    it("removes all listeners", function(done) {
+        var poller = new Poller(function(params, callback) {
+            poller.stop();
+            should(poller.listeners().length).eql(0);
+            callback(null, [ ]);
+            return done();
+        });
         poller.on("message", noop);
-        poller.stop();
-        should(poller.listeners().length).eql(0);
+        poller.start();
     });
 });
